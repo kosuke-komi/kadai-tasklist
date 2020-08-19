2a8245ed422fd15bbd46ad714aab715e1bdeb577
kosuke.komi <ci22183l@gmail.com>
2020-08-19 13:23:48 +0000

    kadai-tasklist

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -50,8 +50,14 @@ class TasksController extends Controller
      // postでmessages/にアクセスされた場合の「新規登録処理」
     public function store(Request $request)
     {
+        // バリデーション
+        $request->validate([
+            'status' => 'required|max:10',   // 追加
+        ]);
+        
         // メッセージを作成
         $task = new Task;
+        $task->status = $request->status;   // 追加
         $task->content = $request->content;
         $task->save();
 
@@ -105,9 +111,15 @@ class TasksController extends Controller
      // putまたはpatchでmessages/（任意のid）にアクセスされた場合の「更新処理」
     public function update(Request $request, $id)
     {
+         // バリデーション
+        $request->validate([
+            'status' => 'required|max:10',   // 追加
+        ]);
+        
         // idの値でメッセージを検索して取得
         $task = Task::findOrFail($id);
         // メッセージを更新
+        $task->status = $request->status;   // 追加
         $task->content = $request->content;
         $task->save();
 
--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -2,12 +2,26 @@
 
 @section('content')
 
+<!--@if (count($errors) > 0)-->
+<!--        <ul class="alert alert-danger" role="alert">-->
+<!--            @foreach ($errors->all() as $error)-->
+<!--                <li class="ml-4">{{ $error }}</li>-->
+<!--            @endforeach-->
+<!--        </ul>-->
+<!--    @endif-->
+<!--app.blade.phpに集約-->
+
 <h1>id: {{ $task->id }} の編集ページ</h1>
 
     <div class="row">
         <div class="col-6">
             {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
-
+            
+                <div class="form-group">
+                    {!! Form::label('status', '状態:') !!}
+                    {!! Form::text('status', null, ['class' => 'form-control']) !!}
+                </div>
+                
                 <div class="form-group">
                     {!! Form::label('content', '内容:') !!}
                     {!! Form::text('content', null, ['class' => 'form-control']) !!}
